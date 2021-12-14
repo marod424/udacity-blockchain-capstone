f31219a5442d684fbb9b8d107e607c6539f63275
matthew <marod424@gmail.com>
2021-12-14 08:37:19 -0500

    tests for ERC721 mintable

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -1,47 +1,64 @@
 var ERC721Mintable = artifacts.require("CustomERC721Token");
 
-contract('TestERC721Mintable', accounts => {
+contract('TestERC721Mintable', async (accounts) => {
+    let contract;
 
-    const account_one = accounts[0];
-    const account_two = accounts[1];
+    const owner = accounts[0];
+    const ape = accounts[1];
 
-    describe('match erc721 spec', function () {
-        beforeEach(async function () { 
-            this.contract = await ERC721Mintable.new("0x Land Title", "ZLT", {from: account_one});
+    describe('match erc721 spec', () => {
 
-            // TODO: mint multiple tokens
-        })
+        beforeEach(async () => { 
+            contract = await ERC721Mintable.new("0x Land Title", "ZLT");
 
-        it('should return total supply', async function () { 
-            
-        })
+            await contract.mint(owner, 1);
+            await contract.mint(owner, 2);
+            await contract.mint(owner, 3);
+        });
 
-        it('should get token balance', async function () { 
-            
-        })
+        it('should return total supply', async () => { 
+            const totalSupply = await contract.totalSupply();
+            assert.equal(totalSupply, 3);
+        });
 
-        // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
-        it('should return token uri', async function () { 
-            
-        })
+        it('should get token balance', async () => { 
+            const tokenBalance = await contract.balanceOf(owner);
+            assert.equal(tokenBalance, 3);
+        });
 
-        it('should transfer token from one owner to another', async function () { 
-            
-        })
+        it('should return token uri', async () => { 
+            const tokenURI = await contract.tokenURI(1);
+            assert.equal(tokenURI, "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1");
+        });
+
+        it('should transfer token from one owner to another', async () => { 
+            await contract.safeTransferFrom(owner, ape, 1);
+            const newOwner = await contract.ownerOf(1);
+            assert.equal(newOwner, ape);
+        });
     });
 
-    describe('have ownership properties', function () {
-        beforeEach(async function () { 
-            this.contract = await ERC721Mintable.new("0x Land Title", "ZLT", {from: account_one});
-        })
+    describe('have ownership properties', () => {
+        beforeEach(async () => { 
+            contract = await ERC721Mintable.new("0x Land Title", "ZLT");
+        });
 
-        it('should fail when minting when address is not contract owner', async function () { 
+        it('should fail when minting when address is not contract owner', async () => { 
+            let reason = '';
             
-        })
+            try {
+                await contract.mint(ape, 1, {from: ape});
+            }
+            catch(error) {
+                reason = error.reason;
+            }
 
-        it('should return contract owner', async function () { 
-            
-        })
+            assert.equal(reason, 'Caller must be owner');
+        });
 
+        it('should return contract owner', async () => { 
+            const contractOwner = await contract.owner();
+            assert.equal(contractOwner, owner);
+        });
     });
 })
